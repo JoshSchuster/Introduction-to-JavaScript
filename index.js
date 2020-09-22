5dcb038a06afb2ac0a8399d3480543edd01a3a5e
JS MacBook Pro <jsmacbookpro@JSs-MacBook-Pro.local>
2020-09-22 16:28:27 -0500

    completes task 1

--- index.js
+++ index.js
@@ -1,25 +1,40 @@
 /************************************************************** Task 1: Warm-up! **************************************************************/
 //Task a: declare a variable called votingAge, console log true if age > 18 (no function required)
+const votingAge = 21;
 
+if (votingAge > 18){
+    console.log(true);
+}else{
+    false;
+}
 
 
 
 
 //Task b: declare a variable and then use a conditional to change the value of that variable based on the value assigned to a second variable (no function required)
+let num1 = 5;
+const num2 = 7; 
 
+if(num1 > num2){
+    num1++;
+}else{
+    num1--;
+}
 
 
 
 
 //Task c: Convert string ("1999") to integer (1999)  (no function required) // hint look up the Number method
-
+let year = "1999";
+year = Number(year);
 
 
 
 
 //Task d: Write a function to multiply a*b 
-
-
+function multiply(a,b){
+    return a*b
+}
 
 
 
